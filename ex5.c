5848be0f251490a12ce864c577c67ae8fae8b767
Rachlewski <yoav.rachlewski@gmail.com>
2025-01-05 22:11:55 +0200

    main

--- ex5.c
+++ ex5.c
@@ -21,24 +21,32 @@ typedef struct Playlist {
 //some recommendations for functions, you may implement however you want
 
 void deleteSong() {
-
+//todo
     printf("Song deleted successfully.\n");
 }
 
 
 
-void playSong() {
-    
+void playSong(Playlist* plqaylist, int songIndex) {
+  //todo
 }
 
 
 
-void freeSong() {
-
+void freeSong(Song* song) {
+    free (song->title);
+    free (song->artist);
+    free (song->lyrics);
+    free (song);
 }
 
-void freePlaylist(P) {
-    
+void freePlaylist(Playlist* playlist, int songsSize){
+    free(playlist->name);
+    for (int i =0; i < songsSize; i++){
+      freeSong(playlist->songs[i]);
+    }
+    free(playlist->songs);
+    free(playlist);
 }
 
 void printPlaylistsMenu() {
@@ -49,12 +57,38 @@ void printPlaylistsMenu() {
 
 
 void sortPlaylist() {
-    
+    //todo
     printf("sorted\n");
 }
 
 
+void watchPlaylists(void) {
+    //TODO
+}
+
+void addPlaylist(void) {
+    //todo
+}
+
+void removePlaylist(void) {
+    //todo
+}
 
 int main() {
-    printf("Goodbye!\n");  
+  int choice =0;
+  while(choice != 4){
+      printPlaylistsMenu();
+      scanf("%d", &choice);
+      switch (choice) {
+        case 1:
+          watchPlaylists();
+          break;
+        case 2:
+            addPlaylist();
+            break;
+        case 3:
+            removePlaylist();
+      }
+    }
+    printf("Goodbye!\n");
 }
